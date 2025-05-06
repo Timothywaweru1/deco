d5024cc5d9e63de4baf4c936a8bcc598236d4c90
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-07 01:59:26 +0300

    worked on adding and del

---
+++ sequences/splice.py
@@ -0,0 +1,22 @@
+# word = "hello"
+# print(word.index("l"))
+# text = "lucrative"
+# print(text.index("t"))
+# smn = "unbelivable"
+# print(smn.index("n"))
+
+
+#adding 
+pple = ["tom","brad","ian"]
+pple.append("peter")
+pple.append("benja")
+pple.insert(1,"mollyne")
+print(pple)
+
+
+#removing
+num = ["1","3","9","10"]
+# for num in range(10):
+#     print(num)
+num.remove("10")
+print(num)
